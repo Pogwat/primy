3fa884c895958ca9a34fdc519ab65e2f8fec8ac1
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-05-25 18:21:59 -0400

    Inital COmmit

---
+++ target/debug/deps/primy-7b0b82b24495c737.d
@@ -0,0 +1,5 @@
+/etc/nixos/code/primy/target/debug/deps/primy-7b0b82b24495c737.d: src/main.rs
+
+/etc/nixos/code/primy/target/debug/deps/primy-7b0b82b24495c737: src/main.rs
+
+src/main.rs:
---
+++ target/debug/primy.d
@@ -0,0 +1,1 @@
+/etc/nixos/code/primy/target/debug/primy: /etc/nixos/code/primy/src/main.rs
